9b38ed8ca782efe5cb7d73f89755b6c0d9079d8d
André devkcud Albanese Junior <patommmmm@proton.me>
2024-05-24 20:35:46 -0300

    Add prettier configs and tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,5 +4,23 @@ export default {
   theme: {
     extend: {}
   },
-  plugins: []
+  daisyui: {
+    logs: false,
+    themes: [
+      {
+        main: {
+          primary: '#3F5DA8',
+          secondary: '#FA67DA',
+          accent: '#FFCC1A',
+          neutral: '#1f2937',
+          'base-100': '#F4F4F4',
+          info: '#2563eb',
+          success: '#22c55e',
+          warning: '#eab308',
+          error: '#b91c1c'
+        }
+      }
+    ]
+  },
+  plugins: [require('daisyui')]
 };
