8276d1ed208938a29d710ccbee450d896938b8db
André devkcud Albanese Junior <patommmmm@proton.me>
2024-05-27 20:42:00 -0300

    Add Curiosity

--- src/lib/components.ts
+++ src/lib/components.ts
@@ -1,4 +1,5 @@
 import Card from './components/Card.svelte';
 import Carrousel from './components/Carrousel.svelte';
+import Curiosity from './components/Curiosity.svelte';
 
-export { Card, Carrousel };
+export { Card, Carrousel, Curiosity };
---
+++ src/lib/components/Curiosity.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+  export let color: 'yellow' | 'pink' | 'blue' = 'blue';
+</script>
+
+<article class="relative w-full max-w-96 rounded-xl bg-white p-4 shadow-lg shadow-black/10">
+  <div
+    class="absolute inset-x-0 -top-6 *:mx-auto *:size-12 *:rounded-full *:p-2 *:shadow-lg *:shadow-black/20"
+    class:yellow={color === 'yellow'}
+    class:pink={color === 'pink'}
+    class:blue={color === 'blue'}
+  >
+    <slot name="icon" />
+  </div>
+
+  <p class="pt-4 text-center">
+    <slot />
+  </p>
+</article>
+
+<style lang="postcss">
+  .yellow {
+    @apply text-accent;
+  }
+
+  .yellow > * {
+    @apply border border-accent;
+  }
+
+  .pink {
+    @apply text-secondary;
+  }
+
+  .pink > * {
+    @apply border border-secondary;
+  }
+
+  .blue {
+    @apply text-primary;
+  }
+
+  .blue > * {
+    @apply border border-primary;
+  }
+</style>
