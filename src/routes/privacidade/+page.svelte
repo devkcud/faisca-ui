e06045af9fbed38ea898714f29e657e26e079a3c
André devkcud Albanese Junior <patommmmm@proton.me>
2024-08-15 23:23:16 -0300

    haha

--- src/routes/privacidade/+page.svelte
+++ src/routes/privacidade/+page.svelte
@@ -10,32 +10,37 @@
   <section class="space-y-4">
     <div>
       <h2 class="text-4xl font-bold text-quaternary">Política e privacidade</h2>
-
-      <p>
-        Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint
-        ad nisi Lorem pariatur mollit ex esse exercitation amet. Nisi anim cupidatat excepteur
-        officia. Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate
-        dolor minim nulla est proident. Nostrud officia pariatur ut officia. Sit irure elit esse ea
-        nulla sunt ex occaecat reprehenderit commodo officia dolor Lorem duis laboris cupidatat
-        officia voluptate. Culpa proident adipisicing id nulla nisi laboris ex in Lorem sunt duis
-        officia eiusmod. Aliqua reprehenderit commodo ex non excepteur duis sunt velit enim.
-        Voluptate laboris sint cupidatat ullamco ut ea consectetur et est culpa et culpa duis.
-      </p>
     </div>
 
     <div>
       <h3 class="text-2xl font-bold text-quaternary">Informações Coletadas</h3>
 
+      <p>
+        O Faísca Científica utiliza as informações coletadas para diversas finalidades, incluindo:
+      </p>
+
       <ul class="ml-8 list-disc">
         <li>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta magnam cum quasi
-          dignissimos accusamus veniam. Ipsam rerum ullam voluptatibus reiciendis. Molestiae veniam
-          id quae rerum recusandae consequatur corporis culpa nam?
+          Fornecer nosso serviço, garantindo que você possa usufruir de todas as funcionalidades
+          disponíveis.
         </li>
         <li>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta magnam cum quasi
-          dignissimos accusamus veniam. Ipsam rerum ullam voluptatibus reiciendis. Molestiae veniam
-          id quae rerum recusandae consequatur corporis culpa nam?
+          Personalizar sua experiência, oferecendo conteúdos e recursos adequados aos seus
+          interesses e necessidades.
+        </li>
+        <li>
+          Manter uma comunicação eficiente, entrando em contato sobre sua conta e nosso serviço,
+          respondendo às suas perguntas e compartilhando informações.
+        </li>
+        <li>
+          Cumprir com leis e regulamentações legais e aplicáveis, preservando a segurança e
+          integridade dos dados dos usuários, utilizando essas informações de forma responsável e
+          conforme a legislação vigente.
+        </li>
+        <li>
+          Implementar medidas de segurança para proteger a plataforma contra ameaças, investigar e
+          prevenir práticas fraudulentas, evitar acessos não autorizados e outras atividades
+          ilegais.
         </li>
       </ul>
     </div>
@@ -43,97 +48,32 @@
     <div>
       <h3 class="text-2xl font-bold text-quaternary">Segurança</h3>
 
-      <ul class="ml-8 list-disc">
-        <li>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta magnam cum quasi
-          dignissimos accusamus veniam. Ipsam rerum ullam voluptatibus reiciendis. Molestiae veniam
-          id quae rerum recusandae consequatur corporis culpa nam?
-        </li>
-        <li>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta magnam cum quasi
-          dignissimos accusamus veniam. Ipsam rerum ullam voluptatibus reiciendis. Molestiae veniam
-          id quae rerum recusandae consequatur corporis culpa nam?
-        </li>
-      </ul>
+      <p>
+        Adotamos práticas alinhadas aos padrões técnicos e regulatórios de mercado para garantir a
+        segurança e privacidade dos seus dados. Os dados que coletamos são tratados de forma
+        anonimizada para que não seja identificado pessoalmente.
+      </p>
     </div>
 
     <div>
       <h3 class="text-2xl font-bold text-quaternary">Compartilhamento de Informações</h3>
 
-      <ul class="ml-8 list-disc">
-        <li>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta magnam cum quasi
-          dignissimos accusamus veniam. Ipsam rerum ullam voluptatibus reiciendis. Molestiae veniam
-          id quae rerum recusandae consequatur corporis culpa nam?
-        </li>
-        <li>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta magnam cum quasi
-          dignissimos accusamus veniam. Ipsam rerum ullam voluptatibus reiciendis. Molestiae veniam
-          id quae rerum recusandae consequatur corporis culpa nam?
-        </li>
-      </ul>
+      <p>
+        A proteção das suas informações pessoais é uma prioridade para o Faísca Científica. O
+        compartilhamento, quando necessário, dessas informações com terceiros segue nossos padrões
+        de segurança estão sujeitos à obrigação de confidencialidade e privacidade.
+      </p>
     </div>
 
     <div>
       <h3 class="text-2xl font-bold text-quaternary">Cookies</h3>
 
-      <ul class="ml-8 list-disc">
-        <li>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta magnam cum quasi
-          dignissimos accusamus veniam. Ipsam rerum ullam voluptatibus reiciendis. Molestiae veniam
-          id quae rerum recusandae consequatur corporis culpa nam?
-        </li>
-        <li>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta magnam cum quasi
-          dignissimos accusamus veniam. Ipsam rerum ullam voluptatibus reiciendis. Molestiae veniam
-          id quae rerum recusandae consequatur corporis culpa nam?
-        </li>
-      </ul>
+      <p>
+        Parte dos dados coletados pela Faísca Científica acontece através de cookies, que permitem
+        entregar um serviço personalizado de acordo com suas necessidades e preferências. Os cookies
+        armazenam suas preferências em dispositivos como computadores e smartphones, proporcionando
+        conteúdo e publicidade específicos para uma experiência online mais enriquecedora.
+      </p>
     </div>
-
-    <div>
-      <h3 class="text-2xl font-bold text-quaternary">Informações de Contato</h3>
-
-      <ul class="ml-8 list-disc">
-        <li>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta magnam cum quasi
-          dignissimos accusamus veniam. Ipsam rerum ullam voluptatibus reiciendis. Molestiae veniam
-          id quae rerum recusandae consequatur corporis culpa nam?
-        </li>
-        <li>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta magnam cum quasi
-          dignissimos accusamus veniam. Ipsam rerum ullam voluptatibus reiciendis. Molestiae veniam
-          id quae rerum recusandae consequatur corporis culpa nam?
-        </li>
-      </ul>
-    </div>
-
-    <div>
-      <h3 class="text-2xl font-bold text-quaternary">Alterações na Política de Privacidade</h3>
-
-      <ul class="ml-8 list-disc">
-        <li>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta magnam cum quasi
-          dignissimos accusamus veniam. Ipsam rerum ullam voluptatibus reiciendis. Molestiae veniam
-          id quae rerum recusandae consequatur corporis culpa nam?
-        </li>
-        <li>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta magnam cum quasi
-          dignissimos accusamus veniam. Ipsam rerum ullam voluptatibus reiciendis. Molestiae veniam
-          id quae rerum recusandae consequatur corporis culpa nam?
-        </li>
-      </ul>
-    </div>
-
-    <p>
-      Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint
-      ad nisi Lorem pariatur mollit ex esse exercitation amet. Nisi anim cupidatat excepteur
-      officia. Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor
-      minim nulla est proident. Nostrud officia pariatur ut officia. Sit irure elit esse ea nulla
-      sunt ex occaecat reprehenderit commodo officia dolor Lorem duis laboris cupidatat officia
-      voluptate. Culpa proident adipisicing id nulla nisi laboris ex in Lorem sunt duis officia
-      eiusmod. Aliqua reprehenderit commodo ex non excepteur duis sunt velit enim. Voluptate laboris
-      sint cupidatat ullamco ut ea consectetur et est culpa et culpa duis.
-    </p>
   </section>
 </MainLayout>
